5230fcc271d1871af7266374daf41092775582ac
arnthor89 <arnthorj2@gmail.com>
2019-02-18 17:04:50 +0100

    #14 [refactor] Change name of functions to keep consistency

--- test/sysfont_test.py
+++ test/sysfont_test.py
@@ -30,49 +30,51 @@ class SysfontModuleTest(unittest.TestCase):
 
     @unittest.skipIf('Linux' not in platform.platform(), 'Not linux we skip.')
     def test_initsysfonts_unix(self):
-        self.assertTrue(len(pygame.sysfont.get_fonts()) > 1)
+        self.assertGreater(len(pygame.sysfont.get_fonts()), 1)
 
     @unittest.skipIf(not FONT, 'No fonts found, skip')
     def test_sysfont_known(self):
         font = pygame.font.SysFont(self.FONT, 40, 1, 1)
+        self.assertIsNotNone(font)
         self.assertTrue(font.get_bold())
         self.assertTrue(font.get_italic())
 
     def test_sysfont_unkown(self):
         font = pygame.sysfont.SysFont(self.UNKOWN_FONT, 40, 1, 1)
+        self.assertIsNotNone(font)
         self.assertTrue(font.get_bold())
         self.assertTrue(font.get_italic())
 
     @unittest.skipIf(not FONT, 'No fonts found, skip')
-    def test_sysfont_setFontName(self):
+    def test_get_font_name(self):
         fontname, bold, italic = pygame.sysfont.get_font_name(self.FONT, 0, 0)  
-        self.assertTrue(fontname)
+        self.assertIsNotNone(fontname)
         self.assertFalse(bold)
         self.assertFalse(italic)
 
     @unittest.skipIf(not FONT, 'No fonts found, skip')
-    def test_sysfont_setFontNameWithStyles(self):
+    def test_get_font_name_with_styles(self):
         fontname = pygame.sysfont.get_font_name(self.FONT, 1, 1)  
-        self.assertTrue(fontname)
+        self.assertIsNotNone(fontname)
 
-    def test_sysfont_setFontNameNoName(self):
+    def test_get_font_name_none(self):
         fontname = pygame.sysfont.get_font_name(None, 0, 0)  
         self.assertIsNone(fontname)
 
     @unittest.skipIf(not FONT, 'No fonts found, skip')
-    def test_sysfont_setNoStyles(self):
+    def test_get_styles(self):
         fontname, bold, italic = pygame.sysfont.get_styles(self.FONT, 0, 0)  
-        self.assertTrue(fontname)
+        self.assertIsNotNone(fontname)
         self.assertFalse(bold)
         self.assertFalse(italic)
 
-    def test_sysfont_setStylesNoName(self):
+    def test_get_styles_none(self):
         fontname, bold, italic = pygame.sysfont.get_styles(None, 0, 0)  
         self.assertIsNone(fontname)
         self.assertFalse(bold)
         self.assertFalse(italic)
 
-    def test_sysfont_setInvalidFont(self):
+    def test_set_styles_unkown(self):
         fontname, bold, italic = pygame.sysfont.get_styles(self.UNKOWN_FONT, 1, 1)  
         self.assertIsNone(fontname)
         self.assertFalse(bold)
